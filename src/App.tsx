07c8e924d161868fc376c02c32ceae4b0eee9fc5
BaileyFirman <bailey.firman@nintex.com>
2024-02-02 19:47:13 +1300

    Add basic styling along with color underline

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 // import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [showProjects, setShowProjects] = useState(false);
+
   return (
     <>
       <div className="app-container">
         <span className="app-header">
-          <h1>bfirman.io</h1>
-          <h3>about</h3>
-          <h3>blog</h3>
-          <h3>projects</h3>
+          <h1 className="app-main-heading">bfirman.io</h1>
+          <span className="five-colors-background" />
         </span>
+        <div className="app-body">
+          <h3 className="app-sub-heading-about">about</h3>
+          <h3 className="app-sub-heading-blog">blog</h3>
+          <h3
+            className="app-sub-heading-projects"
+            onClick={() => setShowProjects((x) => !x)}
+          >
+            projects
+          </h3>
+        </div>
       </div>
     </>
   );
