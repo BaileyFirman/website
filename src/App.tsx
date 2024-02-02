2b5437e31f8dc9a3d0c306487a39969b75a200b0
BaileyFirman <bailey.firman@nintex.com>
2024-02-02 20:00:22 +1300

    Bound app-header size to text width

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [showProjects, setShowProjects] = useState(false);
+  const [_, setShowProjects] = useState(false);
 
   return (
     <>
       <div className="app-container">
-        <span className="app-header">
+        <div className="app-header">
           <h1 className="app-main-heading">bfirman.io</h1>
           <span className="five-colors-background" />
-        </span>
+        </div>
         <div className="app-body">
           <h3 className="app-sub-heading-about">about</h3>
           <h3 className="app-sub-heading-blog">blog</h3>
