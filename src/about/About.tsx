295034c6590dd60e6bce28a8987c0f1ad312000b
BaileyFirman <bailey.firman@nintex.com>
2024-02-03 08:30:03 +1300

    Add about css, and add experience list

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -1,7 +1,19 @@
+import "./About.css";
+
 function About() {
   return (
     <div className="about-container">
-      <h5 className="about-main-heading">Hello World.</h5>
+      <h5 className="about-main-heading">Hi, I'm Bailey</h5>
+      <p className="about-main-content">
+        I'm a software developer from Auckland, New Zealand.
+      </p>
+      <p className="about-main-content">Experience</p>
+      <ul className="about-experience-list">
+        <li>$DAYJOB</li>
+        <li>Intelli.TV</li>
+        <li>MSTRPCE.IO</li>
+        <></>
+      </ul>
     </div>
   );
 }
